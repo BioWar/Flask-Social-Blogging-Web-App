3eb5dbc8d6cf74adb32bd7d078ac78902e75b922
Petr Vor <biowarius@gmail.com>
2020-06-22 13:42:34 +0300

    Added user posts on profile page

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,8 @@ from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
